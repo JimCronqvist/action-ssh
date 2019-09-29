e85d9508e210d21ec6aac69dbd8b955b37d9d54f
Jim Cronqvist <jim.cronqvist@gmail.com>
2019-09-29 16:44:27 +0700

    SSH action implementation

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,15 +13,15 @@ install -m 700 /dev/null ~/script.sh
 echo "${INPUT_COMMAND}" > ~/script.sh
 
 echo ""
-echo "${BLUE}Run on: ${INPUT_HOSTS}${NORMAL}"
-echo "${BLUE}Commands:${NORMAL}"
+echo -e "${BLUE}Run on: ${INPUT_HOSTS}${NORMAL}"
+echo -e "${BLUE}Commands:${NORMAL}"
 cat ~/script.sh
 echo ""
 
 for host in ${INPUT_HOSTS}; do
-  echo "${BLUE}Connecting to ${host}...${NORMAL}"
+  echo -e "${BLUE}Connecting to ${host}...${NORMAL}"
   sh -c "ssh -t -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no '${host}' < ~/script.sh"
 done
 
 echo ""
-echo "${BLUE}GitHub Action completed.${NORMAL}"
+echo -e "${BLUE}GitHub Action completed.${NORMAL}"
