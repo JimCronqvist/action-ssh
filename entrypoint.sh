d5d00cc9452e85686ceea15eb7268225dc3eecd1
Jim Cronqvist <jim.cronqvist@gmail.com>
2019-09-29 16:22:14 +0700

    SSH action implementation

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 set -eu
 
 mkdir -p ~/.ssh/
-install -m 600 "${INPUT_PRIVATEKEY}" ~/.ssh/id_rsa
-install -m 700 "${INPUT_COMMAND}" ~/script.sh
+install -m 600 /dev/null ~/.ssh/id_rsa
+echo "${INPUT_PRIVATEKEY}" > ~/.ssh/id_rsa
+install -m 700 /dev/null ~/script.sh
+echo "${INPUT_COMMAND}" > ~/script.sh
 
 echo "Run on: ${INPUT_HOSTS}"
 echo "Commands:"
