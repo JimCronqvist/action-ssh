aeabd5c2ad2c564ca78f370574163009f03d49f7
Jim Cronqvist <jim.cronqvist@gmail.com>
2019-09-29 16:42:18 +0700

    SSH action implementation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,26 @@
 
 set -eu
 
+BLUE=$([ -t 1 ] && tput setaf 6 || echo '')
+NORMAL=$([ -t 1 ] && tput sgr0 || echo '')
+
+
 mkdir -p ~/.ssh/
 install -m 600 /dev/null ~/.ssh/id_rsa
 echo "${INPUT_PRIVATEKEY}" > ~/.ssh/id_rsa
 install -m 700 /dev/null ~/script.sh
 echo "${INPUT_COMMAND}" > ~/script.sh
 
-echo "Run on: ${INPUT_HOSTS}"
-echo "Commands:"
+echo ""
+echo "${BLUE}Run on: ${INPUT_HOSTS}${NORMAL}"
+echo "${BLUE}Commands:${NORMAL}"
 cat ~/script.sh
 echo ""
 
 for host in ${INPUT_HOSTS}; do
-  echo "Connecting to ${host}..."
+  echo "${BLUE}Connecting to ${host}...${NORMAL}"
   sh -c "ssh -t -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no '${host}' < ~/script.sh"
 done
 
-echo "Completed"
+echo ""
+echo "${BLUE}GitHub Action completed.${NORMAL}"
